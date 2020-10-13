7bf0abd506c9b88d86f84511137bc079e6279b8b
Jason Fayre <jfayre@gmail.com>
2020-10-13 18:07:11 -0400

    commit before switching to test branch

--- source/Autopilot/frmAutopilot.Designer.cs
+++ source/Autopilot/frmAutopilot.Designer.cs
@@ -32,10 +32,10 @@
             this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
             this.lblSetting = new System.Windows.Forms.Label();
             this.txtSetting = new System.Windows.Forms.TextBox();
+            this.chkLock = new System.Windows.Forms.CheckBox();
             this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
             this.btnOk = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
-            this.chkLock = new System.Windows.Forms.CheckBox();
             this.flowLayoutPanel1.SuspendLayout();
             this.flowLayoutPanel2.SuspendLayout();
             this.flowLayoutPanel3.SuspendLayout();
@@ -47,8 +47,9 @@
             this.flowLayoutPanel1.Controls.Add(this.flowLayoutPanel3);
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 100);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(300, 154);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
             // flowLayoutPanel2
@@ -56,43 +57,58 @@
             this.flowLayoutPanel2.Controls.Add(this.lblSetting);
             this.flowLayoutPanel2.Controls.Add(this.txtSetting);
             this.flowLayoutPanel2.Controls.Add(this.chkLock);
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(4, 5);
+            this.flowLayoutPanel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(200, 100);
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(300, 154);
             this.flowLayoutPanel2.TabIndex = 0;
             // 
             // lblSetting
             // 
             this.lblSetting.AutoSize = true;
-            this.lblSetting.Location = new System.Drawing.Point(3, 0);
+            this.lblSetting.Location = new System.Drawing.Point(4, 0);
+            this.lblSetting.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblSetting.Name = "lblSetting";
-            this.lblSetting.Size = new System.Drawing.Size(35, 13);
+            this.lblSetting.Size = new System.Drawing.Size(51, 20);
             this.lblSetting.TabIndex = 0;
             this.lblSetting.Text = "label1";
             // 
             // txtSetting
             // 
-            this.txtSetting.Location = new System.Drawing.Point(44, 3);
+            this.txtSetting.Location = new System.Drawing.Point(63, 5);
+            this.txtSetting.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtSetting.MaxLength = 10;
             this.txtSetting.Name = "txtSetting";
-            this.txtSetting.Size = new System.Drawing.Size(100, 20);
+            this.txtSetting.Size = new System.Drawing.Size(148, 26);
             this.txtSetting.TabIndex = 1;
+            // 
+            // chkLock
+            // 
+            this.chkLock.AutoSize = true;
+            this.chkLock.Location = new System.Drawing.Point(4, 41);
+            this.chkLock.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.chkLock.Name = "chkLock";
+            this.chkLock.Size = new System.Drawing.Size(113, 24);
+            this.chkLock.TabIndex = 2;
+            this.chkLock.Text = "checkBox1";
+            this.chkLock.UseVisualStyleBackColor = true;
             // 
             // flowLayoutPanel3
             // 
             this.flowLayoutPanel3.Controls.Add(this.btnOk);
             this.flowLayoutPanel3.Controls.Add(this.btnCancel);
-            this.flowLayoutPanel3.Location = new System.Drawing.Point(209, 3);
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(312, 5);
+            this.flowLayoutPanel3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.flowLayoutPanel3.Name = "flowLayoutPanel3";
-            this.flowLayoutPanel3.Size = new System.Drawing.Size(200, 100);
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(300, 154);
             this.flowLayoutPanel3.TabIndex = 1;
             // 
             // btnOk
             // 
-            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.btnOk.Location = new System.Drawing.Point(3, 3);
+            this.btnOk.Location = new System.Drawing.Point(4, 5);
+            this.btnOk.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnOk.Name = "btnOk";
-            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.Size = new System.Drawing.Size(112, 35);
             this.btnOk.TabIndex = 0;
             this.btnOk.Text = "Ok";
             this.btnOk.UseVisualStyleBackColor = true;
@@ -101,33 +117,25 @@
             // btnCancel
             // 
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(84, 3);
+            this.btnCancel.Location = new System.Drawing.Point(124, 5);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.Size = new System.Drawing.Size(112, 35);
             this.btnCancel.TabIndex = 1;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             // 
-            // chkLock
-            // 
-            this.chkLock.AutoSize = true;
-            this.chkLock.Location = new System.Drawing.Point(3, 29);
-            this.chkLock.Name = "chkLock";
-            this.chkLock.Size = new System.Drawing.Size(80, 17);
-            this.chkLock.TabIndex = 2;
-            this.chkLock.Text = "checkBox1";
-            this.chkLock.UseVisualStyleBackColor = true;
-            // 
             // frmAutopilot
             // 
             this.AcceptButton = this.btnOk;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.ControlBox = false;
             this.Controls.Add(this.flowLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmAutopilot";

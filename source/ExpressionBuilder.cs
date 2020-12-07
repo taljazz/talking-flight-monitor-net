0eb6d3dd86d6cf7b81ab342ce4f96352f04856a5
Andy Borka <ajborka@users.noreply.github.com>
2020-12-07 13:54:55 -0500

    Added ExpressionBuilder class
    Added an expression builder that takes care of building expressions and lambdas for the filtering features of the airports and other procedure features. The expression builder can also be used to create formulas such as ground altitude, distances not provided by the FSUIPC C# library, and other general calculations and lambda expressions. For those contributers that might use expression builder for formulas, the expression type is mainly used if the formula or calculation is frequently used, or when the source code needs to know how a particular formula is calculated. Example: If all we care about is the ground speed result (120kn), then expression builder might not be appropriate for ground speed. On the other hand, if TFM needs to know how distance is calculated from waypoint to waypoint because the formula might change at some point during the flight, then expression trees are a good solution since we can modify the distance calculation on the fly during the flight.

---
+++ source/ExpressionBuilder.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lambdas
+{
+static internal class ExpressionBuilder<t>
+    {
+
+        static public Expression<Func<t, bool>> BuildFromString(string left, string right, type type)
+        {
+            if (string.IsNullOrWhiteSpace(left))
+            {
+                throw new ArgumentException($"'{nameof(left)}' cannot be null or whitespace", nameof(left));
+            }
+
+            if (string.IsNullOrWhiteSpace(right))
+            {
+                throw new ArgumentException($"'{nameof(right)}' cannot be null or whitespace", nameof(right));
+            }
+
+            var parameter = Expression.Parameter(typeof(t), nameof(t));
+            var _left = Expression.Property(parameter, left);
+            var _right = Expression.Constant(right);
+
+switch(type)
+            {
+                case type.Equals:
+                    var _expression = Expression.Equal(_left, _right);
+                    return Expression.Lambda<Func<t, bool>>(_expression, parameter);
+                                                        case type.Contains:
+                    var _containsMethod = typeof(string).GetMethod("Contains", new[] { typeof(string) });
+ var _containsExpression = Expression.Call(_left, _containsMethod, _right);
+                    return Expression.Lambda<Func<t, bool>>(_containsExpression, parameter);
+                case type.StartsWith:
+                    var _startsWithMethod = typeof(string).GetMethod("StartsWith", new[] { typeof(string) });
+                    var _startsWithExpression = Expression.Call(_left, _startsWithMethod, _right);
+                    return Expression.Lambda<Func<t, bool>>(_startsWithExpression, parameter);
+                case type.EndsWith:
+                    var _endsWithMethod = typeof(string).GetMethod("EndsWith", new[] { typeof(string) });
+                    var _endsWithExpression = Expression.Call(_left, _endsWithMethod, _right);
+                    return Expression.Lambda<Func<t, bool>>(_endsWithExpression, parameter);
+                default:
+                    return null;
+            }
+                    } // End BuildFromString.
+
+        static public Expression<Func<t, bool>> BuildFromDouble(type type, string left, double lower, double upper = 0.0)
+        {
+            if (string.IsNullOrWhiteSpace(left))
+            {
+                throw new ArgumentException($"'{nameof(left)}' cannot be null or whitespace", nameof(left));
+            }
+           
+            var parameter = Expression.Parameter(typeof(t), nameof(t));
+            var _property = Expression.Property(parameter, left);
+            var _lower = Expression.Constant(lower);
+            var _upper = Expression.Constant(upper);
+
+                        switch (type)
+            {
+                case type.Equals:
+                    var _expression = Expression.Equal(_property, _lower);
+                    return Expression.Lambda<Func<t, bool>>(_expression, parameter);
+                case type.GreaterThan:
+                    var _greaterThanExpression = Expression.GreaterThan(_property, _lower);
+                    return Expression.Lambda<Func<t, bool>>(_greaterThanExpression, parameter);
+                case type.LessThan:
+                    var _lessThanExpression = Expression.LessThan(_property, _lower);
+                    return Expression.Lambda<Func<t, bool>>(_lessThanExpression, parameter);
+                case type.Between:
+        if(upper <= lower)
+                    {
+                        throw new ArgumentOutOfRangeException($"{nameof(upper)} is too small.");
+                    }
+                    var _lowerExpression = Expression.GreaterThanOrEqual(_property, _lower);
+                    var _upperExpression = Expression.LessThanOrEqual(_property, _upper);
+                    var _betweenExpression = Expression.AndAlso(_lowerExpression, _upperExpression);
+                    return Expression.Lambda<Func<t, bool>>(_betweenExpression, parameter);
+                default:
+                    return null;
+            }
+        } // End BuildFromDouble.
+
+        static public Expression<Func<t, bool>> BuildFromInt(type type, string left, int lower, int upper = 0)
+        {
+            if (string.IsNullOrWhiteSpace(left))
+            {
+                throw new ArgumentException($"'{nameof(left)}' cannot be null or whitespace", nameof(left));
+            }
+
+            var parameter = Expression.Parameter(typeof(t), typeof(t).ToString());
+            var _property = Expression.Property(parameter, left);
+            var _lower = Expression.Constant(lower);
+            var _upper = Expression.Constant(upper);
+
+            switch (type)
+            {
+                case type.Equals:
+                    var _expression = Expression.Equal(_property, _lower);
+                    return Expression.Lambda<Func<t, bool>>(_expression, parameter);
+                case type.GreaterThan:
+                    var _greaterThanExpression = Expression.GreaterThan(_property, _lower);
+                    return Expression.Lambda<Func<t, bool>>(_greaterThanExpression, parameter);
+                case type.LessThan:
+                    var _lessThanExpression = Expression.LessThan(_property, _lower);
+                    return Expression.Lambda<Func<t, bool>>(_lessThanExpression, parameter);
+                case type.Between:
+                    if (upper <= lower)
+                    {
+                        throw new ArgumentOutOfRangeException($"{nameof(upper)} is too small.");
+                    }
+                    var _lowerExpression = Expression.GreaterThanOrEqual(_property, _lower);
+                    var _upperExpression = Expression.LessThanOrEqual(_property, _upper);
+                    var _betweenExpression = Expression.AndAlso(_lowerExpression, _upperExpression);
+                    return Expression.Lambda<Func<t, bool>>(_betweenExpression, parameter);
+                default:
+                    return null;
+            }
+        } // End BuildFromInt.
+        public enum type
+        {
+            StartsWith,
+            EndsWith,
+            Contains,
+            Between,
+            Equals,
+            LessThan,
+            GreaterThan,
+        }
+        
+
+    }
+}
